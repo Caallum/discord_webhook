3224c479ba255637d571c0807d8af3b1f2848651
Tur-ph <67241029+Tur-ph@users.noreply.github.com>
2021-04-17 17:53:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const url = 'WEBHOOK URL';
 const Webhook = require('./webhook.js');
+const discord = require('discord.js');
 const webhook = new Webhook(url)
   .setUsername('Hello World!')
-  .setIcon('https://github.com/Tur-ph/discord_webhook/blob/a61502dd1795d689c298ab363eea71e955f5d1f1/assets/profile_picture.jpg')
+  .setIcon('https://github.com/Tur-ph/discord_webhook/blob/a61502dd1795d689c298ab363eea71e955f5d1f1/assets/profile_picture.jpg');
 
-webhook.send('Tutorial Message', { title: 'Tutorial Message', description: 'Tutorial Message' });
+const embed = new discord.MessageEmbed()
+  .setTitle('Tutorial Message')
+  .setDescription('Tutorial Message');
+
+webhook.send('Tutorial Message', embed);
